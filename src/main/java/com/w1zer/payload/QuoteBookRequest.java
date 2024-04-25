08877ec8b974196730e3a44da224edc53a14ca8a
W1zeR <sergey_novikov_1995@inbox.ru>
2024-04-25 17:38:27 +0300

    .

--- src/main/java/com/w1zer/payload/QuoteBookRequest.java
+++ src/main/java/com/w1zer/payload/QuoteBookRequest.java
@@ -1,10 +1,14 @@
 package com.w1zer.payload;
 
+import com.w1zer.validation.NotBlankIfPresent;
+import jakarta.validation.Valid;
+
 import java.util.Set;
 
 public record QuoteBookRequest(
+        @NotBlankIfPresent
         String title,
 
-        Set<BookAuthorRequest> authors
+        Set<@Valid BookAuthorRequest> authors
 ) {
 }
