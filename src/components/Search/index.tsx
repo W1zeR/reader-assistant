6e8188716734660ad6c8479538748863363f2f3e
Sergey Lavrov <sergey_novikov_1995@inbox.ru>
2024-05-21 20:55:34 +0300

    .

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -31,7 +31,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
       </label>
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2
-        placeholder:text-gray-500"
+        placeholder:text-gray-500 dark:placeholder:text-white"
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value);
@@ -39,7 +39,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500
-      peer-focus:text-gray-900" />
+      peer-focus:text-gray-900 dark:text-gray-300 dark:peer-focus:text-white" />
     </div>
   );
 }
